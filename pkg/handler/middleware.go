d007f9dcecd9c760952e6034180a4040f60c3cc3
Almas Serik <s.almas_99@mail.ru>
2023-01-24 23:04:44 +0600

    middleware redirect to sign in page if user unauthorized

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -14,14 +14,16 @@ const (
 func (h *Handler) userIdentity(c *gin.Context) {
 	token, err := c.Cookie("token")
 	if err != nil {
-		newErrorResponse(c, http.StatusUnauthorized, err.Error())
-		return
+		c.Redirect(http.StatusSeeOther, "/auth/sign-in")
+		// newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		// return
 	}
 
 	userId, err := h.services.Authorization.ParseToken(token)
 	if err != nil {
-		newErrorResponse(c, http.StatusUnauthorized, err.Error())
-		return
+		c.Redirect(http.StatusSeeOther, "/auth/sign-in")
+		// newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		// return
 	}
 
 	c.Set(userCtx, userId)
