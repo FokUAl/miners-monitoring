88adb074a117ceb99bff5afedd57e1d65f4e05e1
Almas Serik <s.almas_99@mail.ru>
2023-01-24 22:28:34 +0600

    adding test users in database migrations

---
+++ schema/000002_testUsers.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM users WHERE username = "Nick" or username = "Aibek";
---
+++ schema/000002_testUsers.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (email, username, password_hash) VALUES ("joker@example.org", "Nick", 
+    "4b617a616b687374616e466f72657665727c222fb2927d828af22f592134e8932480637c0d");
+
+INSERT INTO users (email, username, password_hash) VALUES ("tester@example.org", "Aibek", 
+    "4b617a616b687374616e466f7265766572b1b3773a05c0ed0176787a4f1574ff0075f7521e");
