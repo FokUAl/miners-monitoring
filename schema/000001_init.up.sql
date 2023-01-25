db678844a0ad943697fd2d6f69af209555b30340
Almas Serik <s.almas_99@mail.ruj>
2023-01-25 23:06:12 +0600

    adding new test data in database

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -19,3 +19,17 @@ CREATE TABLE miner_devices
     mac_address VARCHAR(255) NOT NULL,
     _pool VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE all_devices
+(
+    id SERIAL PRIMARY KEY,
+    miner_type VARCHAR(255) NOT NULL,
+    shelf INTEGER DEFAULT 0,
+    _row INTEGER DEFAULT 0,
+    col INTEGER DEFAULT 0,
+    miner_status VARCHAR(255) NOT NULL,
+    coin VARCHAR(255) NOT NULL,
+    ip_address VARCHAR(255) NOT NULL,
+    mac_address VARCHAR(255) NOT NULL,
+    _pool VARCHAR(255) NOT NULL
+);
---
+++ schema/000003_testDevices.down.sql
@@ -0,0 +1,13 @@
+DELETE FROM all_devices WHERE ip_address = '172.168.10.10';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.1';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.2';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.3';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.5';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.4';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.6';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.7';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.11';
+
+DELETE FROM miner_devices WHERE ip_address = '172.168.10.10';
+DELETE FROM miner_devices WHERE ip_address = '172.168.10.5';
+DELETE FROM all_devices WHERE ip_address = '172.168.10.11';
