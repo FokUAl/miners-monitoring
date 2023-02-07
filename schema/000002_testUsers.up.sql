1a7d2487334ae13e784ccc0a54839efc6eb919c1
FokUAl <alibek007007@gmail.com>
2023-02-07 23:45:29 +0600

    new page asic

--- schema/000002_testUsers.up.sql
+++ schema/000002_testUsers.up.sql
@@ -3,3 +3,6 @@ INSERT INTO users (username, role_, email, password_hash) VALUES ('Nick', 'Opera
 
 INSERT INTO users (username, role_, email, password_hash) VALUES ('Aibek', 'Admin', 'tester@example.org',
     '4b617a616b687374616e466f7265766572b1b3773a05c0ed0176787a4f1574ff0075f7521e');
+
+INSERT INTO users (username, role_, email, password_hash) VALUES ('Carl', 'User', 'carl@example.org',
+    '4b617a616b687374616e466f7265766572de187642e6c75f60d10f29e52cab54cdf676870d');
