c85a8bd382be7341c2dc8f8283d3e3731b2420b2
FokUAl <alibek007007@gmail.com>
2023-02-15 14:16:56 +0600

    Frontend addDevice form made

--- web/src/components/AddDeviceForm.js
+++ web/src/components/AddDeviceForm.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FormService from '../services/form.service'
 
 export default function AddDeviceForm() {
+    const [ IP, setIP ] = useState()
+    const [ shelf, setShelf ] = useState()
+    const [ column, setColumn ] = useState()
+    const [ raw, setRaw ] = useState()
+
+    const handleAdd = async(e) => {
+        e.preventDefault()
+        FormService.addDevice(IP, shelf, column, raw).then(
+            (error) => { console.log(error)}
+        )
+    }
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={handleAdd}>
                 <div class="form--title">Add new Device</div>
                 <div class="form--inputs">
-                    <select name="model">
-                        <option value=""></option>
-                    </select>
-                    <div className="connections">
-                        <input name="connection" type="radio" />
-                        <input name="connection" type="radio" />
-                    </div>
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
+                    <label>IP</label>
+                    <input type="text" value={IP} onChange={e => setIP(e.target.value)} required/>
+                    <label>Shelf</label>
+                    <input type="text" value={shelf} onChange={e => setShelf(e.target.value)} required/>
+                    <label>Column</label>
+                    <input type="text" value={column} onChange={e => setColumn(e.target.value)} required/>
+                    <label>Raw</label>
+                    <input type="text" value={raw} onChange={e => setRaw(e.target.value)} required/>
+                    <input type="submit" />
                 </div>
             </form>
         </div>
