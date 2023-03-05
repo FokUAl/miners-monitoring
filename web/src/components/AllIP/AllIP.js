0d4b5dd6706755c3b819b614f9e5791398a2fc0c
FokUAl <alibek007007@gmail.com>
2023-03-05 11:17:17 +0600

    loading animation

--- web/src/components/AllIP/AllIP.js
+++ web/src/components/AllIP/AllIP.js
@@ -1,35 +1,43 @@
-import { useState, useEffect } from 'react'
-import ComponentService from '../../services/component.service'
+import { useState, useEffect } from 'react';
+import ComponentService from '../../services/component.service';
+import Container from '../Container/Container';
+import './allIP.scss';
 
-export default function AllIP () {
-    const [ data, setData ] = useState()
-    const [ loading, setLoading] = useState(true)
+export default function AllIP() {
+	const [data, setData] = useState();
+	const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        ComponentService.getAllIP().then(
-            response => {
-                console.log(response)
-                setData(response.data.List)
-                console.log('allIP ok ');
-                setLoading(true)
-            },
-            error => {
-                console.log('allIP error', error);
-            }
-        );
-    }, []);
+	useEffect(() => {
+		ComponentService.getAllIP().then(
+			(response) => {
+				console.log(response);
+				setData(response.data.List);
+				console.log('allIP ok ');
+				setLoading(true);
+			},
+			(error) => {
+				console.log('allIP error', error);
+			}
+		);
+	}, []);
 
-    // const dataArr = data.map(el => {
-    //     return (
-    //         <div>{el}</div>
-    //     )
-    // })    
+	// const dataArr = data.map(el => {
+	//     return (
+	//         <div>{el}</div>
+	//     )
+	// })
 
-    return (
-        <div>
-            {loading && {data}}
-            {/* {dataArr} */}
-            {data}
-        </div>
-    )
+	return (
+		<div>
+			{loading ? (
+				<Container>
+					<div className="loader-container">
+						<div className="spinner"></div>
+					</div>
+				</Container>
+			) : (
+				<Container>{data}</Container>
+			)}
+		</div>
+	);
 }
