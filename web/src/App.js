0862409e3bf648d58ceb03c181f495af414c1252
FokUAl <alibek007007@gmail.com>
2023-03-08 16:45:27 +0600

    Handling expiring Token

--- web/src/App.js
+++ web/src/App.js
@@ -1,67 +1,79 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom' 
-import Home from './pages/Home'
-import AddDevice from './pages/AddDevice'
-import Grid from './pages/Grid'
-import SignIn from './pages/Auth/SignIn'
-import SignUp from './pages/Auth/SignUp'
-import AuthService from './services/auth.service'
-import PrivateRoute from './PrivateRoute'
-import Device from './pages/Device'
-import "./scss/app.scss"
-import PageService from './services/page.service'
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import Home from './pages/Home';
+import AddDevice from './pages/AddDevice';
+import Grid from './pages/Grid';
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
+import AuthService from './services/auth.service';
+import PrivateRoute from './PrivateRoute';
+import Device from './pages/Device';
+import './scss/app.scss';
+import PageService from './services/page.service';
+import jwt_decode from 'jwt-decode';
 
 function App() {
-  const location = useLocation()
-  const navigation = useNavigate()
-  useEffect(() => {
-    PageService.getHome().catch((error) => {
-      if (error) {
-        console.log('Auth error ', error)
-        AuthService.logout()
-        navigation('/auth/signIn')
-      }
-    })
-  }, [location.pathname])
-  return (
-    <div className="App">
-      <Routes>
-        <Route 
-          path="/auth/signIn" 
-          element={<SignIn />} 
-        />
-        <Route 
-          path="/auth/signUp" 
-          element={<SignUp />} 
-        />
+	const location = useLocation();
+	const navigation = useNavigate();
 
-        <Route 
-          path='/' 
-          element={
-            <PrivateRoute><Home /></PrivateRoute>
-          } 
-        />
-        <Route 
-          path='/addDevice' 
-          element={
-            <PrivateRoute><AddDevice /></PrivateRoute>
-          } 
-        />
-        <Route 
-          path='/grid' 
-          element={
-            <PrivateRoute><Grid /></PrivateRoute>
-          } 
-        />
-        <Route 
-          path='/device' 
-          element={
-            <PrivateRoute><Device /></PrivateRoute>
-          } 
-        />
-      </Routes>
-    </div>
-  );
+	useEffect(() => {
+		const token = AuthService.getCurrentUser();
+		if (token) {
+			const decodedToken = jwt_decode(token);
+      const currentTime = new Date()
+			if (decodedToken.exp * 1000 < currentTime.getTime()) {
+				console.log('Token is expired');
+				AuthService.logout();
+				navigation('/auth/signIn');
+			}
+		} else {
+			console.log('There is no token');
+			AuthService.logout();
+			navigation('/auth/signIn');
+		}
+	}, [location.pathname]);
+
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/auth/signIn" element={<SignIn />} />
+				<Route path="/auth/signUp" element={<SignUp />} />
+
+				<Route
+					path="/"
+					element={
+						<PrivateRoute>
+							<Home />
+						</PrivateRoute>
+					}
+				/>
+				<Route
+					path="/addDevice"
+					element={
+						<PrivateRoute>
+							<AddDevice />
+						</PrivateRoute>
+					}
+				/>
+				<Route
+					path="/grid"
+					element={
+						<PrivateRoute>
+							<Grid />
+						</PrivateRoute>
+					}
+				/>
+				<Route
+					path="/device"
+					element={
+						<PrivateRoute>
+							<Device />
+						</PrivateRoute>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
