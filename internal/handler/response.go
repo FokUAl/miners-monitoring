f686ee2c9c34eecf650d91a5c3913fcab02350cf
Almas Serik <s.almas_99@mail.ru>
2023-03-10 15:24:38 +0600

    Revert "Delete unused code"
    This reverts commit 73d7daad435e1e71200e9b8e57df39f85e5b9e8e.

--- internal/handler/response.go
+++ internal/handler/response.go
@@ -7,6 +7,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	log.Printf("ERROR %s\n", message)
 	c.JSON(statusCode, struct {
