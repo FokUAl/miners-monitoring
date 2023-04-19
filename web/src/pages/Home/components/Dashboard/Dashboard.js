3509f9896b9d16a85b50408c50af1205be8c541a
FokUAl <alibek007007@gmail.com>
2023-04-19 11:06:13 +0600

    Dashboard statuses added

--- web/src/pages/Home/components/Dashboard/Dashboard.js
+++ web/src/pages/Home/components/Dashboard/Dashboard.js
@@ -2,24 +2,21 @@ import React from 'react';
 import Container from '@components/Container/Container';
 import ReData from './ReData';
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
+import DataDisplay from '@pages/Device/components/DeviceInfo/DataDisplay'
 import './dashboard.scss';
 
 export default function Dashboard({ devices }) {
-	const { onlineCount } = ReData(devices);
+	const { onlineCount, totalHashrate, avgTemp } = ReData(devices);
 	const chartData = [
 		{ name: 'Online', value: onlineCount },
 		{ name: 'Offline', value: devices.length - onlineCount },
 	];
-    const colors = ['#76a15e', '#c50000']
+	const colors = ['#76a15e', '#c50000'];
+	console.log(devices);
 	return (
 		<Container paddingLeft borderBottom>
 			<div className="dash--title">Dashboard</div>
-			<div className="grid-50-50">
-				<div>
-					{onlineCount}/{devices.length}
-				</div>
-				<div>{onlineCount / devices.length}%</div>
-			</div>
+			<div className="grid-50-50 m-tp">
 				<PieChart width={730} height={250}>
 					<Pie
 						data={chartData}
@@ -29,14 +26,23 @@ export default function Dashboard({ devices }) {
 						cy="50%"
 						outerRadius={50}
 						fill="#8884d8"
-                        label
+						label
 					>
-                    {chartData.map((entry, index) => (
-                      <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-                    ))}
-                    </Pie>
-                    <Tooltip />
+						{chartData.map((entry, index) => (
+							<Cell
+								key={`cell-${index}`}
+								fill={colors[index % colors.length]}
+							/>
+						))}
+					</Pie>
+					<Tooltip />
 				</PieChart>
+				<div className='m-rt m-tp'>
+					<DataDisplay className text={'Total Hashrate'} data={totalHashrate} />
+                    <DataDisplay className text={'Miners Status'} data={`${onlineCount}/${devices.length}`} />
+                    <DataDisplay className text={'Average Temperature'} data={avgTemp} />
+				</div>
+			</div>
 		</Container>
 	);
 }
--- web/src/pages/Home/components/Dashboard/ReData.js
+++ web/src/pages/Home/components/Dashboard/ReData.js
@@ -2,8 +2,9 @@ const ReData = ({ devices }) => {
 	const onlineCount = devices
 		? devices.filter((device) => device.MinerStatus === 'online').length
 		: 0;
-	console.log(onlineCount);
-	return { onlineCount };
+    const totalHashrate = devices ? devices.reduce((accum, device) => accum + device.Characteristics.MHSav) : '0'
+	const avgTemp = devices ? devices.reduce((accum, device) => accum + device.Characteristics.Temperature) / devices.length : '0'
+	return { onlineCount , totalHashrate, avgTemp};
 };
 
 export default ReData;
