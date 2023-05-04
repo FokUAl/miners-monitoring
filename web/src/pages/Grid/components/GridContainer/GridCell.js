9cd086e007e4f7cd22a6c3016d9e81201002cf6f
FokUAl <alibek007007@gmail.com>
2023-05-04 13:40:25 +0600

    HeatMap done

--- web/src/pages/Grid/components/GridContainer/GridCell.js
+++ web/src/pages/Grid/components/GridContainer/GridCell.js
@@ -8,9 +8,9 @@ export default function GridCell({ children, deviceChar, type }) {
 		if (deviceChar && type === 'onlineMap') {
 			classes.push('status-' + deviceChar.MinerStatus);
 		} else if (deviceChar && type === 'heatMap') {
-			if (deviceChar.Characteristics.ChipTempMax > 69) classes.push('status-normal')
-			if (deviceChar.Characteristics.ChipTempMax > 80) classes.push('status-heat')
-			if (deviceChar.Characteristics.ChipTempMax < 70) classes.push('status-cold')
+			if (deviceChar.Characteristics.ChipTempMax > 64) classes.push('status-normal')
+			if (deviceChar.Characteristics.ChipTempMax > 90) classes.push('status-heat')
+			if (deviceChar.Characteristics.ChipTempMax < 65) classes.push('status-cold')
 			if (deviceChar.Characteristics.ChipTempMax === 0) classes.push('status-notfound')
 		} else {
 			classes.push('status-undefined');
