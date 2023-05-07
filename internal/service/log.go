92fd042c3852d465965cfc19bd7055f605a2ce67
Almas Serik <s.almas_99@mail.ru>
2023-05-07 13:26:07 +0600

    func for reading kernel log in service layer

---
+++ internal/service/log.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// type LogService struct {
+// 	repo repository.Log
+// }
+
+// func NewLogService(repo repository.Log) *LogService {
+// 	return &LogService{
+// 		repo: repo,
+// 	}
+// }
+
+func (s *InfoService) GetKernelLog(ip string) string {
+	req, err := http.NewRequest(http.MethodGet, "http://"+ip+"/cgi-bin/get_kernel_log.cgi?_=1683435164627", nil)
+	if err != nil {
+		return ""
+	}
+
+	req.Header = http.Header{
+		"Accept":          {"text/plain", "q=0.01"},
+		"Accept-Encoding": {"gzip", "deflate"},
+		"Accept-Language": {"en-US,en", "q=0.05"},
+		"Authorization": {`Digest username="root"`, `realm="antMiner Configuration"`,
+			`nonce="78ace61c9cbe541d7551d6b5d434c22d"`, `uri="/cgi-bin/get_kernel_log.cgi?_=1683437716716"`,
+			`response="7fff99ae5afe79613396cd032e0afb61"`, `qop=auth`, `nc=00000137`,
+			`cnonce="ea5de4d8ec2b3120"`},
+	}
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(content)
+}
